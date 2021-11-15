bbb4f2c0baf0cca2f3a2c05cdd1eb5607605369f
S5330524 <92516250+S5330524@users.noreply.github.com>
2021-11-15 07:51:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <iostream> 
 
-int main() {
-   return 0;
+uing namespase std;
+
+
+Int main ()
+{
+     Int n=0, i=1,  a=0, b=1, c; 
+
+     cin>>n; 
+
+     cout<<b<<endl; 
+
+     for(i=0,i<n,i++)
+     {
+     c= a+b;
+cout<<c<<endl;
+a=b;
+b=c; 
+
+      }
+return 0;
 }
