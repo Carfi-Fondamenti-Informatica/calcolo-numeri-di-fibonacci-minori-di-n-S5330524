fc0bbd3fd8fd433ab0bbac319254e8212b5d9f0c
S5330524 <92516250+S5330524@users.noreply.github.com>
2021-11-15 07:58:38 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespase std;
 
 Int main ()
 {
-     Int n=0, i=1,  a=0, b=1, c; 
+     Int n=0,i=1,b=1,a=0,c; 
 
      cin>>n; 
 
@@ -14,9 +14,9 @@ Int main ()
      for(i=0,i<n,i++)
      {
      c= a+b;
-cout<<c<<endl;
-a=b;
-b=c; 
+     cout<<c<<endl;
+     a=b;
+     b=c; 
 
       }
 return 0;
